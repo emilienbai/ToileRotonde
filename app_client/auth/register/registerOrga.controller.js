791a0fe5e513a28041c995b108f3cb2470cfc9c9
emilienbai <emilien.bai@insa-lyon.fr>
2017-02-18 18:51:17 +0100

    First commit with passport authentication

---
+++ app_client/auth/register/registerOrga.controller.js
@@ -0,0 +1,33 @@
+(function () {
+
+    angular
+        .module('meanApp')
+        .controller('registerCtrl', registerCtrl);
+
+    registerCtrl.$inject = ['$location', 'authentication'];
+    function registerCtrl($location, authentication) {
+        var vm = this;
+
+        vm.credentials = {
+            name: "",
+            email: "",
+            phone: "",
+            password: "",
+            accountType:"Orga"
+        };
+
+        vm.onSubmit = function () {
+            console.log('Submitting registration');
+            authentication
+                .register(vm.credentials)
+                .error(function (err) {
+                    alert(err);
+                })
+                .then(function () {
+                    $location.path('profile');
+                });
+        };
+
+    }
+
+})();
